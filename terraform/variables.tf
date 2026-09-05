97b1c034c3e292504be858671251baf0cd8cadb3
Joao Costa <jpoc.joaopedro@gmail.com>
2026-09-06 00:39:11 +0100

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "cloudflare_api_token" {
+  type        = string
+  description = "The API Token for Cloudflare account authentication with Pages Edit permissions"
+  sensitive   = true
+}
+
+variable "cloudflare_account_id" {
+  type        = string
+  description = "Your 32-character Cloudflare Account ID"
+}
+
+variable "github_username" {
+  type        = string
+  description = "Your GitHub username"
+  default     = "JoaoCosta"
+}
+
+variable "github_repo_name" {
+  type        = string
+  description = "The name of your public portfolio repository"
+}
+
+variable "custom_domain" {
+  type        = string
+  description = "The custom domain you want to link to your portfolio (e.g., joaocosta.com)"
+  default     = ""
+}
